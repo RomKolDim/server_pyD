c96408615d0dc1112236304ea31419040506dd6b
RomKolDim <165240653+RomKolDim@users.noreply.github.com>
2026-02-16 10:42:24 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+import psycopg2
+from flask import Flask
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
+    )
+
+@app.route("/")
+def index():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS demo (
+            id SERIAL PRIMARY KEY,
+            name TEXT
+        );
+        """)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return "Connected to DB container!"
+
+    except Exception as e:
+        return f"DB connection error: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
